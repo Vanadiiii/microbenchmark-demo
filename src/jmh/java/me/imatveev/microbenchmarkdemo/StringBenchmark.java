07c3d818f98c0d9fc101b00b97818107babb815c
MatveevIV <MatveevIV4@mos.ru>
2023-03-18 11:33:28 +0300

    small fixes

--- src/jmh/java/me/imatveev/microbenchmarkdemo/StringBenchmark.java
+++ src/jmh/java/me/imatveev/microbenchmarkdemo/StringBenchmark.java
@@ -9,6 +9,8 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -21,8 +23,8 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 @Fork(value = 1)
-@Warmup(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
-@Measurement(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
+@Warmup(iterations = 3, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
+@Measurement(iterations = 3, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
 public class StringBenchmark {
     private static final int COUNT = 10_000;
 
@@ -64,7 +66,7 @@ public class StringBenchmark {
 
     public static void main(String[] args) throws RunnerException {
         Options options = new OptionsBuilder()
-//                .addProfiler(GCProfiler.class)
+                .addProfiler(GCProfiler.class)
                 .include(".*" + StringBenchmark.class.getSimpleName() + ".*")
                 .build();
         new Runner(options).run();
